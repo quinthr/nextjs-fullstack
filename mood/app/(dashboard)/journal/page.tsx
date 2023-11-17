95f49dabbbd17cae1deac1be4508c7ad399c7ceb
Quinth Anthony A. Razuman <31249145+quinthr@users.noreply.github.com>
2023-11-17 22:33:44 +0800

    Journal entries

--- mood/app/(dashboard)/journal/page.tsx
+++ mood/app/(dashboard)/journal/page.tsx
@@ -1,3 +1,5 @@
+import EntryCard from '@/components/EntryCard'
+import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
@@ -19,7 +21,15 @@ const JournalPage = async () => {
   const entries = await getEntries()
   return (
     <>
-      <div>Journal</div>
+      <div className="p-10 bg-zinc-400/10 h-full">
+        <h2 className="text-3xl mb-8">Journal</h2>
+        <div className="grid grid-cols-3 gap-4">
+          <NewEntryCard />
+          {entries.map((entry) => (
+            <EntryCard entry={entry} key={entry.id} />
+          ))}
+        </div>
+      </div>
     </>
   )
 }
--- mood/app/(dashboard)/layout.tsx
+++ mood/app/(dashboard)/layout.tsx
@@ -7,13 +7,13 @@ const DashboardLayout = ({ children }) => {
         <aside className="absolute top-0 w-[200px] left-0 h-full border-r border-black/10">
           Mood
         </aside>
-        <div className="ml-[200px]">
+        <div className="ml-[200px] h-full">
           <header className="h-[60px] border-b border-black/10">
             <div className="h-full w-full flex px-6 items-center justify-end">
               <UserButton />
             </div>
           </header>
-          <div>{children}</div>
+          <div className="h-[calc(100vh-60px)]">{children}</div>
         </div>
       </div>
     </>
